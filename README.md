097cecab3f831e12e9d28e39d417e9a08b9a7179
DavidKielty1 <davidkielty@live.co.uk>
2021-03-09 19:40:35 +0000

    Wooot

---
+++ README.md
@@ -0,0 +1,4 @@
+# SearchApp1
+Search API with SASS
+
+Progressed with the SASS imports.
---
+++ js/main.js
@@ -0,0 +1,43 @@
+import { setSearchFocus, showClearTextButton } from "./searchBar.js";
+import {
+  deleteSearchResults,
+  buildSearchResults,
+  clearStatsLine,
+  setStatsLine,
+} from "./searchResults.js";
+import { getSearchTerm, retrieveSearchResults } from "./dataFunctions.js";
+
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  setSearchFocus();
+
+  // 3 listeners clear text
+  const search = document.getElementById("search");
+  search.addEventListener("input", showClearTextButton);
+
+  const form = document.getElementById("searchBar");
+  form.addEventListener("submit", submitTheSearch);
+};
+
+// Procedural "workflow" function
+const submitTheSearch = (event) => {
+  event.preventDefault();
+  deleteSearchResults();
+  processTheSearch();
+  setSearchFocus();
+};
+
+//Procedural
+const processTheSearch = async () => {
+  clearStatsLine();
+  const searchTerm = getSearchTerm();
+  if (searchTerm === "") return;
+  const resultArray = await retrieveSearchResults(searchTerm);
+  if (resultArray.length) buildSearchResults(resultArray);
+  setStatsLine(resultArray.length);
+};
